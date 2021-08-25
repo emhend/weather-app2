6db98d71126d00f3d1bd5207aa7ede4a8676cc03
emhend <86583654+emhend@users.noreply.github.com>
2021-08-24 23:16:53 -0500

    Added CSS edits, JS functions

--- src/index.js
+++ src/index.js
@@ -20,9 +20,25 @@ function formatDate(date) {
   return `${days[dayIndex]} ${hours}:${minutes}`;
 }
 function displayWeather(response) {
+  fahrenheitTemperature = response.data.main.temp;
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML =
     Math.round(response.data.main.temp) + "°";
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML =
+    "Wind: " + Math.round(response.data.wind.speed) + "MPH";
+  let precipitationElement = document.querySelector("#precipitation");
+  precipitationElement.innerHTML =
+    "Precipitation: " + Math.round(response.data.main.rain);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 function searchCity(city) {
   let apiKey = "343e26b3ffb2c17a61d6a1123defd48c";
@@ -43,6 +59,19 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let celsiusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature) + "°";
+}
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature) + "°";
+}
+
+let fahrenheitTemperature = null;
 
 let dateElement = document.querySelector("#current-date");
 let date = new Date();
@@ -55,3 +84,9 @@ let currentLocation = document.querySelector("#current");
 currentLocation.addEventListener("click", getCurrentLocation);
 
 searchCity("Austin");
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
